80831dbeeb961799da0f7f3dd92a9e57896dd8b0
Kasper Henningsen <khenni20@student.aau.dk>
2021-11-05 13:54:17 +0100

    Health Report window WIP
    Health report window created. The tab ram and cpu is created. Network tab is not started!
    overall missing visual update.

---
+++ DashboardFrontend/DetachedWindows/HealthReportDetached.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DashboardFrontend.DeatachedWindows
+{
+    public partial class HealthReportDetached : Window
+    {
+        public HealthReportDetached()
+        {
+            InitializeComponent();
+        }
+    }
+}
