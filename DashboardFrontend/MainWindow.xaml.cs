838b2a729fd57cfa0b96ff67da583ed559b116d1
Kasper Henningsen <khenni20@student.aau.dk>
2021-11-05 09:50:57 +0100

    Made the interface independent

---
+++ DashboardFrontend/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace DashboardInterface
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            //DialogWindow dialogWindow = new();
+            //dialogWindow.Owner = Application.Current.MainWindow;
+            //dialogWindow.ShowDialog();
+        }
+
+        //Expand window events
+        public void SettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+            //SettingsWindow settingsWindow = new();
+            //settingsWindow.Closing += OnSettingsWindowClosing;
+            //settingsWindow.IsEnabled = false;
+            //settingsWindow.Owner = Application.Current.MainWindow;
+            //settingsWindow.ShowDialog();
+        }
+
+        public void ExpandManagerButtonClick(object sender, RoutedEventArgs e)
+        {
+            //ManagerWindow expandManager = new();
+            //expandManager.Closing += OnManagerWindowClosing;
+            //managerExpandButton.IsEnabled = false;
+            //expandManager.Owner = Application.Current.MainWindow;
+            //expandManager.Show();
+        }
+
+        public void ExpandLogButtonClick(object sender, RoutedEventArgs e)
+        {
+            //LogWindow expandLog = new();
+            //expandLog.Closing += OnLogWindowClosing;
+            //logExpandButton.IsEnabled = false;
+            //expandLog.Owner = Application.Current.MainWindow;
+            //expandLog.Show();
+        }
+
+        public void ExpandValidationReportButtonClick(object sender, RoutedEventArgs e)
+        {
+            //ValidationReportWindow expandVR = new();
+            //expandVR.Closing += OnValidationWindowClosing;
+            //validationReportExpandButton.IsEnabled = false;
+            //expandVR.Owner = Application.Current.MainWindow;
+            //expandVR.Show();
+        }
+
+        public void ExpandHealthReportButtonClick(object sender, RoutedEventArgs e)
+        {
+            //HealthReportWindow expandHR = new();
+            //healthReportExpandButton.IsEnabled = false;
+            //expandHR.Closing += OnHealthWindowClosing;
+            //expandHR.Owner = Application.Current.MainWindow;
+            //expandHR.Show();
+        }
+
+        //OnWindowClosing events
+        private void OnSettingsWindowClosing(object? sender, CancelEventArgs e)
+        {
+            settingsButton.IsEnabled = true;
+        }
+
+        private void OnManagerWindowClosing(object sender, CancelEventArgs e)
+        {
+            managerExpandButton.IsEnabled = true;
+        }
+
+        private void OnLogWindowClosing(object sender, CancelEventArgs e)
+        {
+            logExpandButton.IsEnabled = true;
+        }
+
+        private void OnValidationWindowClosing(object sender, CancelEventArgs e)
+        {
+            validationReportExpandButton.IsEnabled = true;
+        }
+
+        private void OnHealthWindowClosing(object sender, CancelEventArgs e)
+        {
+            healthReportExpandButton.IsEnabled = true;
+        }
+    }
+}
