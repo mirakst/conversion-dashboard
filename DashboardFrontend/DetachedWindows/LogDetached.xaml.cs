9d11b0e6a0d63e2d05aae3c7d26ff9f62a62eafb
Torbjørn <thelst20@student.aau.dk>
2021-11-10 14:53:13 +0100

    Added colouring depending on error type

--- DashboardFrontend/DetachedWindows/LogDetached.xaml.cs
+++ DashboardFrontend/DetachedWindows/LogDetached.xaml.cs
@@ -1,12 +1,30 @@
 ﻿using System.Windows;
+using System.Collections.Generic;
 
 namespace DashboardFrontend.DetachedWindows
 {
+    public class LogEntry
+    {
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public long Manager { get; set; }
+        public string Content { get; set; }
+    }
+
     public partial class LogDetached : Window
     {
         public LogDetached()
         {
             InitializeComponent();
+            
+            
+            dataGridLogEntry.Items.Add(new LogEntry() { Content = "Det her er en besked", Type = "Info", Manager = 2, Date = "10:11:2021"});
+            dataGridLogEntry.Items.Add(new LogEntry() { Content = "Det her er en advarsel", Type = "Warn", Manager = 2, Date = "10:11:2021" });
+            dataGridLogEntry.Items.Add(new LogEntry() { Content = "Det her er en fejl", Type = "Error", Manager = 2, Date = "10:11:2021" });
+            dataGridLogEntry.Items.Add(new LogEntry() { Content = "Det her er en TOTAL DESTRUKTION ", Type = "Fatal", Manager = 2, Date = "10:11:2021" });
+            dataGridLogEntry.Items.Add(new LogEntry() { Content = "Det her er en validation ", Type = "Validation", Manager = 3, Date = "10/11:2021" });
+
+            
         }
     }
 }
