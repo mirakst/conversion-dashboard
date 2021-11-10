976bae35ed74314da581d199753855adaf2f4a9a
AndersMariegaard <Amarie20@student.aau.dk>
2021-11-10 12:55:12 +0100

    Added charts and performance controls
    Finished UI for now

--- DashboardFrontend/DetachedWindows/ManagerListDetached.xaml.cs
+++ DashboardFrontend/DetachedWindows/ManagerListDetached.xaml.cs
@@ -14,6 +14,13 @@ using System.Windows.Shapes;
 
 namespace DashboardFrontend.DetachedWindows
 {
+    public class item
+    {
+        public string name { get; set; }
+        public int score {  get; set; }
+        public string status { get; set; }
+        public int id {  get; set; }
+    }
     /// <summary>
     /// Interaction logic for ManagerListDetached.xaml
     /// </summary>
@@ -22,6 +29,14 @@ namespace DashboardFrontend.DetachedWindows
         public ManagerListDetached()
         {
             InitializeComponent();
+            datagridManagers.Items.Add(new item() {name = "Assashdoaishdoahsdaasdaadssadasdasd", score = 3, status = "fail", id = 1023});
+            datagridManagers.Items.Add(new item() {name = "usdagsgssdfsf", score = 412, status = "OK", id = 1231 });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            datagridDetailedManagers.Items.Add(e.OriginalSource);
+            datagridChartedManagers.Items.Add(e.OriginalSource);
         }
     }
 }
