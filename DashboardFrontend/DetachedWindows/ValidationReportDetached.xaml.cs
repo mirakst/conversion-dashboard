8b8b01aa865c548cff034a0070f8d7b910dfeb38
Markus F. T <mthage20@student.aau.dk>
2021-11-11 16:14:39 +0100

    Basic Skeleton of validation entry added

--- DashboardFrontend/DetachedWindows/ValidationReportDetached.xaml.cs
+++ DashboardFrontend/DetachedWindows/ValidationReportDetached.xaml.cs
@@ -14,11 +14,34 @@ using System.Windows.Shapes;
 
 namespace DashboardFrontend.DetachedWindows
 {
+
+    public class ValidationEntry
+    {
+        public string ManagerName { get; set; } 
+        public int ValidationsOK { get; set; }  
+        public int ValidationsTotal { get; set; }
+        public double ManagerScore { get; set; }  
+    }
     public partial class ValidationReportDetached : Window
     {
+
         public ValidationReportDetached()
         {
             InitializeComponent();
+            List<ValidationEntry> inputlist = new List<ValidationEntry>();
+
+            inputlist.Add(new ValidationEntry() { ManagerName = "OneCleanDude", ValidationsOK = 2, ValidationsTotal = 4, ManagerScore = 250.75 });
+            inputlist.Add(new ValidationEntry() { ManagerName = "FullSickoMode", ValidationsOK = 2, ValidationsTotal = 4, ManagerScore = 250.75 });
+            inputlist.Add(new ValidationEntry() { ManagerName = "OneEvenMoreCleanDude", ValidationsOK = 2, ValidationsTotal = 4, ManagerScore = 250.75 });
+            inputlist.Add(new ValidationEntry() { ManagerName = "HellSickoMode", ValidationsOK = 2, ValidationsTotal = 4, ManagerScore = 250.75 });
+
+            foreach (ValidationEntry validationEntry in inputlist)
+            {
+                dataGridValidationReport.Items.Add(validationEntry);
+            }
+
         }
     }
 }
+
+
