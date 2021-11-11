f4eaacbe926acca6c5a288739129bc08de28ad53
AndersMariegaard <Amarie20@student.aau.dk>
2021-11-11 13:37:12 +0100

    Added visual adjustments and functionality to UI elements

--- DashboardFrontend/DetachedWindows/ManagerListDetached.xaml.cs
+++ DashboardFrontend/DetachedWindows/ManagerListDetached.xaml.cs
@@ -14,19 +14,19 @@ using System.Windows.Shapes;
 
 namespace DashboardFrontend.DetachedWindows
 {
-    public class item
+    public class Item
     {
-        public string name { get; set; }
-        public int score {  get; set; }
-        public string status { get; set; }
-        public int id {  get; set; }
-        public int execId {  get; set; }
-        public int run {  get; set; }
-        public int startTime {  get; set; }
-        public int endTime {  get; set; }
+        public string? Name { get; set; }
+        public int Score {  get; set; }
+        public string? Status { get; set; }
+        public int ID {  get; set; }
+        public int ExecId {  get; set; }
+        public int Run {  get; set; }
+        public int StartTime {  get; set; }
+        public int EndTime {  get; set; }
         public int Runtime {  get; set; }
-        public int rowsRead {  get; set; }
-        public int rowsWritten {  get; set; }
+        public int RowsRead {  get; set; }
+        public int RowsWritten {  get; set; }
     }
     /// <summary>
     /// Interaction logic for ManagerListDetached.xaml
@@ -36,16 +36,66 @@ namespace DashboardFrontend.DetachedWindows
         public ManagerListDetached()
         {
             InitializeComponent();
-            datagridManagers.Items.Add(new item() {name = "Assashdoaishdoahsdaasdaadssadasdasd", score = 3, status = "fail", id = 1023});
-            datagridManagers.Items.Add(new item() {name = "usdagsgssdfsf", score = 412, status = "OK", id = 1231 });
+            datagridManagers.Items.Add(new Item() {Name = "Assashdoaishdoahsdaasdaadssadasdasd", Score = 3, Status = "fail", ID = 1023, Run = 4 });
+            datagridManagers.Items.Add(new Item() {Name = "usdagsgssdfsf", Score = 412, Status = "OK", ID = 1231, Runtime = 1013});
+            datagridManagers.Items.Add(new Item() { Name = "usdagsgssdfsf", Status = "OK", ID = 2112, Runtime = 3453, RowsRead = 112003 });
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void AddManager_Click(object sender, RoutedEventArgs e)
         {
-            item? obj = ((FrameworkElement)sender).DataContext as item;
-            datagridManagers.Items.Add(obj);
-            datagridDetailedManagers.Items.Add(obj);
-            datagridChartedManagers.Items.Add(obj); 
+            Item? selectedManager = datagridManagers.SelectedItem as Item;
+
+            if (datagridManagers.SelectedItems.Count > 1)
+            {
+                foreach (Item manager in datagridManagers.SelectedItems)
+                {
+                    if (!datagridManagerDetails.Items.Contains(manager))
+                    {
+                        datagridManagerDetails.Items.Add(manager);
+                        datagridManagerCharts.Items.Add(manager);
+                    }
+                }
+            }
+            else
+            {
+                _ = datagridManagerDetails.Items.Add(selectedManager);
+                _ = datagridManagerCharts.Items.Add(selectedManager);
+            }
+        }
+
+        private void RemoveManager_Click(object sender, RoutedEventArgs e)
+        {
+            Item? selectedManager = ((Button)sender).Tag as Item;
+            List<Item> managers = new() { };
+            
+            if (datagridManagerDetails.SelectedItems.Count > 1 || datagridManagerCharts.SelectedItems.Count > 1)
+            {
+                foreach (Item manager in TabInfo.IsSelected == true ? datagridManagerDetails.SelectedItems : datagridManagerCharts.SelectedItems)
+                {
+                    managers.Add(manager);
+                }
+                foreach (Item manager in managers)
+                {
+                    datagridManagerDetails.Items.Remove(manager);
+                    datagridManagerCharts.Items.Remove(manager);
+                }
+            }
+            else
+            {
+                datagridManagerDetails.Items.Remove(selectedManager);
+                datagridManagerCharts.Items.Remove(selectedManager);
+            }
+        }
+
+        private void SearchManagers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetManagers_Click(object sender, RoutedEventArgs e)
+        {
+            datagridManagerDetails.Items.Clear();
+            datagridManagerCharts.Items.Clear();
         }
     }
 }
