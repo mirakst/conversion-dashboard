22081d93c7f6b46caf47f1638a05fad0aa526642
Torbjørn <thelst20@student.aau.dk>
2021-11-12 10:32:57 +0100

    Changed filtering function

--- DashboardFrontend/DetachedWindows/LogDetached.xaml.cs
+++ DashboardFrontend/DetachedWindows/LogDetached.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace DashboardFrontend.DetachedWindows
 {
     
     public class LogEntry
     {
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string Type { get; set; }
         public long Manager { get; set; }
         public string Content { get; set; }
@@ -26,7 +26,6 @@ namespace DashboardFrontend.DetachedWindows
     public partial class LogDetached : Window
     {
         List<LogEntry> logList = new List<LogEntry>();
-        List<LogEntry> filteredList = new List<LogEntry>();
 
         public LogDetached()
         {
@@ -37,15 +36,33 @@ namespace DashboardFrontend.DetachedWindows
         }
         public void AddItems()
         {
-            logList.Add(new LogEntry() { Content = "Det her er en besked", Type = "Info", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en advarsel", Type = "Warn", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en fejl", Type = "Error", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en TOTAL DESTRUKTION", Type = "Fatal", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en validation", Type = "Validation", Manager = 3, Date = "10/11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en endnu besked", Type = "Info", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en  asneiubesked", Type = "Info", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en edadvarsel", Type = "Warn", Manager = 2, Date = "10:11:2021" });
-            logList.Add(new LogEntry() { Content = "Det her er en efefeffejl", Type = "Error", Manager = 2, Date = "10:11:2021" });
+            logList.Add(new LogEntry() { Content = "Det her er en besked", Type = "Info", Manager = 2, Date =  DateTime.Now});
+            logList.Add(new LogEntry() { Content = "Det her er en advarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en fejl", Type = "Error", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en TOTAL DESTRUKTION", Type = "Fatal", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en validation", Type = "Validation", Manager = 3, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en endnu besked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en  asneiubesked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en edadvarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en efefeffejl", Type = "Error", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en besked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en advarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en fejl", Type = "Error", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en TOTAL DESTRUKTION", Type = "Fatal", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en validation", Type = "Validation", Manager = 3, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en endnu besked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en  asneiubesked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en edadvarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en efefeffejl", Type = "Error", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en besked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en advarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en fejl", Type = "Error", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en TOTAL DESTRUKTION", Type = "Fatal", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en validation", Type = "Validation", Manager = 3, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en endnu besked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en  asneiubesked", Type = "Info", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en edadvarsel", Type = "Warn", Manager = 2, Date = DateTime.Now });
+            logList.Add(new LogEntry() { Content = "Det her er en efefeffejl", Type = "Error", Manager = 2, Date = DateTime.Now });
         }
 
         public void PrintList(List<LogEntry> inputList)
@@ -53,18 +70,36 @@ namespace DashboardFrontend.DetachedWindows
             dataGridLogEntry.Items.Clear();
             foreach (LogEntry logEntry in inputList)
             {
-
-                dataGridLogEntry.Items.Add(logEntry);
-
+                switch (logEntry.Type)
+                {
+                    case "Info":
+                        if (LogFilter.ShowInfo)
+                            dataGridLogEntry.Items.Add(logEntry);
+                        break;
+                    case "Warn":
+                        if (LogFilter.ShowWarn)
+                            dataGridLogEntry.Items.Add(logEntry);
+                        break;
+                    case "Error":
+                        if (LogFilter.ShowError)
+                            dataGridLogEntry.Items.Add(logEntry);
+                        break;
+                    case "Fatal":
+                        if (LogFilter.ShowFatal)
+                            dataGridLogEntry.Items.Add(logEntry);
+                        break;
+                    case "Validation":
+                        if (LogFilter.ShowValidation)
+                            dataGridLogEntry.Items.Add(logEntry);
+                        break;
+                }
             }
-            
         }
         
-        
-        public void OnToggleButtonCheck(object sender, RoutedEventArgs e)
+        public void OnToggleButtonClick(object sender, RoutedEventArgs e)
         {
-            var button = (FrameworkElement)sender;
-            string logType = (string)button.Tag;
+            var logToggleButton = (FrameworkElement)sender;
+            string logType = (string)logToggleButton.Tag;
             
             switch (logType)
             {
@@ -84,29 +119,8 @@ namespace DashboardFrontend.DetachedWindows
                     LogFilter.ShowValidation = !LogFilter.ShowValidation;
                     break;
             }
-            
-        }
 
-        /*
-        public void toggleButtonChecked(object sender, RoutedEventArgs e)
-        {
-            var fuckinglort = (FrameworkElement)sender;
-            string logType = (string)fuckinglort.Tag;
-            filteredList.AddRange(logList.Where(e => e.Type == logType).ToList());
-            PrintList(filteredList);
+            PrintList(logList); 
         }
-        public void OnToggleButtonUnCheck(object sender, RoutedEventArgs e)
-        {
-            var fuckinglort = (FrameworkElement)sender;
-            string logType = (string)fuckinglort.Tag;
-            filteredList = filteredList.Where(e => e.Type != logType).ToList();
-            PrintList(filteredList);
-        }
-
-        */
-        
-        
-        
-        
     }
 }
