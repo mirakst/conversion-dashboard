fa7bd8577a83ebe1c19b6a1b803f2256e4dab1ec
Anders Mariegaard <Amarie20@student.aau.dk>
2021-11-23 11:25:36 +0100

    Removed or commented IDD and added better readability to more XAML files

--- DashboardFrontend/StyleResources/CustomLabelProvider.cs
+++ DashboardFrontend/StyleResources/CustomLabelProvider.cs
@@ -1,37 +1,37 @@
-﻿using InteractiveDataDisplay.Core;
-using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
+﻿//using InteractiveDataDisplay.Core;
+//using System;
+//using System.Collections.Generic;
+//using System.Windows;
+//using System.Windows.Controls;
 
-namespace DashboardFrontend.StyleResources
-{
-    public class CustomLabelProvider : ILabelProvider
-    {
-        private static readonly DateTime Origin = new(2000, 1, 1);
+//namespace DashboardFrontend.StyleResources
+//{
+//    public class CustomLabelProvider : ILabelProvider
+//    {
+//        private static readonly DateTime Origin = new(2000, 1, 1);
 
-        public FrameworkElement[] GetLabels(double[] ticks)
-        {
-            if (ticks == null)
-                throw new ArgumentNullException(nameof(ticks));
+//        public FrameworkElement[] GetLabels(double[] ticks)
+//        {
+//            if (ticks == null)
+//                throw new ArgumentNullException(nameof(ticks));
 
-            List<TextBlock> labels = new();
-            foreach (double tick in ticks)
-            {
-                TextBlock text = new();
-                var time = Origin + TimeSpan.FromTicks((long)tick);
-                text.Text = time.ToLongTimeString();
-                labels.Add(text);
+//            List<TextBlock> labels = new();
+//            foreach (double tick in ticks)
+//            {
+//                TextBlock text = new();
+//                var time = Origin + TimeSpan.FromTicks((long)tick);
+//                text.Text = time.ToLongTimeString();
+//                labels.Add(text);
 
-            }
-            return labels.ToArray();
-        }
-    }
+//            }
+//            return labels.ToArray();
+//        }
+//    }
 
-    public class CustomAxis : Axis
-    {
-        public CustomAxis() : base(new CustomLabelProvider(), new TicksProvider())
-        {
-        }
-    }
-}
+//    public class CustomAxis : Axis
+//    {
+//        public CustomAxis() : base(new CustomLabelProvider(), new TicksProvider())
+//        {
+//        }
+//    }
+//}
