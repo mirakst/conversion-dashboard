9e7f8ecc6f9ca9ca11b74cd6b5352b24eaf3bdbd
Mikkel <mstids20@student.aau.dk>
2021-11-30 13:19:20 +0100

    Added N/A value converter

---
+++ DashboardFrontend/ValueConverters/NotFoundToNaConverter.cs
@@ -0,0 +1,21 @@
+﻿using DashboardFrontend.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DashboardFrontend.ValueConverters
+{
+    public class NotFoundToNaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int rowNum = (int)value;
+            return rowNum < 0 ? "N/A" : rowNum;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DashboardFrontend/ViewModels/ManagerViewModel.cs
+++ DashboardFrontend/ViewModels/ManagerViewModel.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
